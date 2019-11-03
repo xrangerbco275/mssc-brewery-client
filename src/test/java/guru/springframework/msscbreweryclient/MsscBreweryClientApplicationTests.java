7b0017d21af954df7d7593b7c91f8fb66b8733e2
xrangerbco275 <xrangerbco275@att.net>
2019-11-03 10:13:30 -0600

    Initial Commit

---
+++ src/test/java/guru/springframework/msscbreweryclient/MsscBreweryClientApplicationTests.java
@@ -0,0 +1,15 @@
+package guru.springframework.msscbreweryclient;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBreweryClientApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
